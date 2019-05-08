c0a8fe1af1b3a94860de4d4c5cefd2d70233d12c
Jason Jewik <jj11d7t@gmail.com>
2019-05-07 18:11:27 -0700

    Dynamically sized section header

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ function resize() {
      var aboutText = document.getElementsByClassName('about-text');
      var projectsHeading = document.getElementById('projects-heading');
      var projectsSubHeading = document.getElementById('projects-subheading');
+     var photoHeading = document.getElementsByClassName('photos-heading');
 
      if (width > height) // screen is landscape oriented
      {
@@ -19,6 +20,7 @@ function resize() {
           aboutText[1].style.fontSize = "2.5vw";
           projectsHeading.style.fontSize = "4vw";
           projectsSubHeading.style.fontSize = "2vw";
+          photosHeading.style.fontSize = "4vw";
      } else // screen is portrait oriented
      {
           landingText.style.fontSize = "8vh";
@@ -26,5 +28,6 @@ function resize() {
           aboutText[1].style.fontSize = "4vh";
           projectsHeading.style.fontSize = "5vh";
           projectsSubHeading.style.fontSize = "3vh";
+          photosHeading.style.fontSize = "5vh";
      }
 }
