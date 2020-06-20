c7f87e017c1246f033d9c863002b3bee65db25d9
Jason Jewik <jj11d7t@gmail.com>
2020-06-20 11:53:19 -0700

    Added page wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,15 @@ import React from 'react'
 import { ThemeProvider } from './src/context/theme-context'
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
+  <div
+    style={{
+      width: '100vw',
+      minHeight: '100vh',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+    }}
+  >
+    <ThemeProvider>{element}</ThemeProvider>
+  </div>
 )
