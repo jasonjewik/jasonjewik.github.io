24042f6692dc0b90a622bc0e35338cbddfaf2a3e
Jason Jewik <jj11d7t@gmail.com>
2020-06-20 11:42:06 -0700

    Create global theme context

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
+import ThemeContext from '../context/theme-context'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,23 +25,27 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <ThemeContext.Consumer>
+      {theme => (
+        <div className={theme.dark ? 'dark' : 'light'}>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0 1.0875rem 1.45rem`,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </div>
+      )}
+    </ThemeContext.Consumer>
   )
 }
 
---
+++ src/context/theme-context.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const defaultState = {
+  dark: false,
+  toggleDark: () => {},
+}
+
+const ThemeContext = React.createContext(defaultState)
+
+class ThemeProvider extends React.Component {
+  state = {
+    dark: false,
+  }
+
+  toggleDark = () => {
+    let dark = !this.state.dark
+    this.setState({ dark })
+  }
+
+  componentDidMount() {
+    const currentHour = new Date().getHours()
+    const isNight = currentHour < 7 || currentHour > 19
+    this.setState({ dark: isNight })
+  }
+
+  render() {
+    const { children } = this.props
+    const { dark } = this.state
+
+    return (
+      <ThemeContext.Provider
+        value={{
+          dark,
+          toggleDark: this.toggleDark,
+        }}
+      >
+        {children}
+      </ThemeContext.Provider>
+    )
+  }
+}
+
+export default ThemeContext
+export { ThemeProvider }
