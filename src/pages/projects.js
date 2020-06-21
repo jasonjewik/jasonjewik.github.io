8eeefc9f7ae7137b7711733a6c30cfef6392bcb5
Jason K. Jewik <jason.jewik@gmail.com>
2020-06-21 12:06:16 -0700

    Projects pages

---
+++ src/pages/projects.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import SEO from '../components/seo'
+
+const NotFoundPage = () => (
+  <>
+    <SEO title="Projects" />
+    <h1>My Projects</h1>
+    <p>
+      Will finish this page up at some future time, but for now, you can find me
+      on <a href="https://github.com/jasonjewik">GitHub</a> and{' '}
+      <a href="https://medium.com/@jasonjewik">Medium</a>.{' '}
+    </p>
+  </>
+)
+
+export default NotFoundPage
